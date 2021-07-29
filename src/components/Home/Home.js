2048d97ac9c8fa6281b8fb5c8321deaa4f7fca82
Steven Moyano <stevenmoyano1@gmail.com>
2021-07-29 16:22:08 -0500

    added front end files for movie app

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+//imports react
+import React, { Component } from "react";
+
+//renders jsx
+export class Home extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "15%", fontSize: 75 }}>
+        Welcome to Coolest APP
+      </div>
+    );
+  }
+}
+
+export default Home;
